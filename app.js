458670bcd6436e9f8ec177b10c1da6b299653891
painterhalver <hieptrang13042002@gmail.com>
2022-11-03 10:11:05 +0700

    Delete long comments, convert jquery to vanilla js

--- app.js
+++ app.js
@@ -6,30 +6,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hjs");
-app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //////////////////////////////////////////////////////////////////////////////
 
-app.get("/", function (req, res, next) {
-  res.render("index", {
-    base_url: process.env.BASE_URL,
-  });
-});
 app.use("/video", require("./routes/video"));
 
 //////////////////////////////////////////////////////////////////////////////
-
-//
-//
-//  If nonce of the above routes matches, we create an error to let the
-//  user know that the URL accessed doesn't match anything.
-//
 app.use(function (req, res, next) {
   let err = new Error("Not Found");
   err.status = 404;
@@ -37,78 +24,18 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-//
 //  Display any error that occurred during the request.
-//
 app.use(function (err, req, res, next) {
-  //
-  //	1.	Set the basic information about the error, that is going to be
-  //		displayed to user and developers regardless.
-  //
   let obj_message = {
     message: err.message,
   };
 
-  //
-  //	2.	Check if the environment is development, and if it is we
-  //		will display the stack-trace
-  //
   if (process.env.NODE_ENV == "development") {
-    //
-    //	1.	Set the variable to show the stack-trace to the developer
-    //
     obj_message.error = err;
-
-    //
-    //	-> Show the error in the console
-    //
     console.error(err);
   }
 
-  //
-  //	3.	Display a default status error, or pass the one from
-  //		the error message
-  //
-  res.status(err.status || 500);
-
-  //
-  //	->	Show the error
-  //
-  res.json(obj_message);
+  res.status(err.status || 500).json(obj_message);
 });
-
-//   _    _ ______ _      _____  ______ _____   _____
-//  | |  | |  ____| |    |  __ \|  ____|  __ \ / ____|
-//  | |__| | |__  | |    | |__) | |__  | |__) | (___
-//  |  __  |  __| | |    |  ___/|  __| |  _  / \___ \
-//  | |  | | |____| |____| |    | |____| | \ \ ____) |
-//  |_|  |_|______|______|_|    |______|_|  \_\_____/
-//
-
-//
-//	Check if the connection is secure, if not, redirect to a secure one.
-//
-function force_https(req, res, next) {
-  //
-  //	1. 	Redirect only in the production environment
-  //
-  if (process.env.NODE_ENV == "production") {
-    //
-    //	1. 	Check what protocol are we using
-    //
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      //
-      //	-> 	Redirect the user to the same URL that he requested, but
-      //		with HTTPS instead of HTTP
-      //
-      return res.redirect("https://" + req.get("host") + req.url);
-    }
-  }
-
-  //
-  //	2. 	If the protocol is already HTTPS the, we just keep going.
-  //
-  next();
-}
 
 module.exports = app;
--- routes/video.js
+++ routes/video.js
@@ -8,11 +8,12 @@ const WebTorrent = require("webtorrent");
 
 const trackers = require("../utils/trackers");
 
-// magnet:?xt=urn:btih:54908313d54387de2948d73080b8d808b6c7eb2b&dn=Spider-Man.No.Way.Home.2021.1080p.BluRay.H264.AAC-RARBG&tr=http%3A%2F%2Ftracker.trackerfix.com%3A80%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2790&tr=udp%3A%2F%2F9.rarbg.to%3A2910&tr=udp%3A%2F%2Ftracker.fatkhoala.org%3A13720&tr=udp%3A%2F%2Ftracker.slowcheetah.org%3A14730
+// Sintel
+// magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent
 
 const router = express.Router();
 
-const client = new WebTorrent({ downloadLimit: 5000000 });
+const client = new WebTorrent({ downloadLimit: 7000000 });
 
 let stats = {
   progress: 0,
@@ -41,27 +42,32 @@ router.get("/add/:magnet", (req, res) => {
   // old_magnets.forEach((magnet) => client.remove(magnet));
   // console.log("CLEARED OLD MAGNETS");
 
-  client.add(
-    magnet,
-    {
-      announce: trackers,
-      store: require("memory-chunk-store"),
-    },
-    function (torrent) {
-      let files = [];
-      torrent.files.forEach(function (file) {
-        files.push({
-          name: file.name,
-          length: file.length,
+  try {
+    client.add(
+      magnet,
+      {
+        announce: trackers,
+        store: require("memory-chunk-store"),
+      },
+      function (torrent) {
+        let files = [];
+        torrent.files.forEach(function (file) {
+          files.push({
+            name: file.name,
+            length: file.length,
+          });
         });
-      });
-      torrent.on("error", (error) => {
-        console.log("ADD MAGNET ERROR: ", error);
-      });
+        torrent.on("error", (error) => {
+          console.log("ADD MAGNET ERROR: ", error);
+        });
 
-      res.json(files);
-    }
-  );
+        res.json(files);
+      }
+    );
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.get("/stream/:magnet/:file_name", (req, res, next) => {
